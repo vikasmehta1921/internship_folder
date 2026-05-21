26088de1c1d4543c6010679be053c953e18b860b
Vikas Mehta <vikasmehta1921@gmail.com>
2026-05-21 23:07:50 +0530

    Added Day4 Python tasks

---
+++ day4/qn1.py
@@ -0,0 +1,13 @@
+def maximum(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+print("Maximum number is:", maximum(num1, num2, num3))
